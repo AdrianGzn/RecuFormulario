7f8a498ae145166da9078c2d5ebb2a4cb7c82135
Regiber18 <regiber.montoya2005@hotmail.com>
2024-10-01 21:33:38 -0600

    change

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,7 @@
 function Button(props) {
     return (
         <button className="w-full h-10 bg-blue-600 text-white rounded mt-2" onClick={props.onClick}>
-            {props.text}
+            <p>hola</p>
         </button>
     );
 }
